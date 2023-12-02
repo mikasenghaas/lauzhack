cd50d9a0ed3411e8df8d0f39c478166e2419e2ba
Mika Senghaas <mail@mikasenghaas.de>
2023-12-02 21:06:34 +0100

    fix(git) merge conflict

--- journey.py
+++ journey.py
@@ -17,12 +17,72 @@ def main():
     if args.intermodal:
         # Convert address to location
         start_loc = utils.get_location(args.start)
+        print(f"Start location: {start_loc}")
 
         # 1. Find k-closest stations with PR
-        # 2.1 Compute distance from start to each station
+        distances = [
+            (end[0], utils.get_distance(start_loc, f"{end[2]}, {end[3]}"))
+            for _, end in pr_stations.iterrows()
+        ]
+        # for i in distances:
+        #    print(i)
+
+        k = args.limit
+        distances.sort(key=lambda t: t[1])
+        k_closest_stations = distances[:k]
+
+        # 2.1 Compute distance from start to each station in seconds
+        car_distances = [
+            (
+                station[0],
+                utils.getRouteTime(
+                    start_loc, utils.get_location(station[0]), method="foot"
+                ),
+            )
+            for station in k_closest_stations
+        ]
+
         # 2.2 Compute journey (top-k) from each possible start to end
+        travels = []
+        for station, dist in car_distances:
+            print(f"Computing journey from {station} to {args.end}")
+            connections = sbb.get_connections(
+                station,
+                args.end,
+                via=args.via,
+                date=args.date,
+                time=args.time,
+                transportations=args.transportations,
+                limit=1,
+            )
+            for connection in connections:
+                # print(connection)
+                travels.append(
+                    {
+                        "start": args.start,
+                        "station": station,
+                        "seconds_to_station": dist,
+                        "end": args.end,
+                        "seconds_from_station_to_end": connection.duration.seconds,
+                        "connection": connection,
+                    }
+                )
+
         # 3. Combine journeys
         # 4. Rank journey and return top-k
+        travels.sort(
+            key=lambda t: t["seconds_to_station"] + t["seconds_from_station_to_end"]
+        )
+        best_travel = travels[0]
+
+        print(
+            f"The fastes journey is:\n"
+            f"1) Drive from {best_travel['start']} to {best_travel['station']} in {best_travel['seconds_to_station']} s\n"
+            f"2) Take the train from {best_travel['station']} to {best_travel['end']} in {best_travel['seconds_from_station_to_end']} s\n"
+            f"\t{best_travel['connection']}\n"
+            f"Total time: {best_travel['seconds_to_station'] + best_travel['seconds_from_station_to_end']} s"
+        )
+
         pass
 
     else:
--- utils.py
+++ utils.py
@@ -2,20 +2,29 @@
 Some utility functions.
 """
 
+import re
+import csv
 import argparse
+import requests
 from pydoc import describe
 import requests
 import json
+from geopy.geocoders import Nominatim
+from geopy.distance import distance
 
 from datetime import date, datetime
+
+PR_STATIONS = "./data/pr_stations"
 
 
 def get_args():
     parser = argparse.ArgumentParser()
 
     # Default date and time args
-    current_date = date.today().strftime("%Y-%m-%d")
-    current_time = datetime.now().strftime("%H:%M")
+    # current_date = date.today().strftime("%Y-%m-%d")
+    # current_time = datetime.now().strftime("%H:%M")
+    current_date = "2023-12-5"
+    current_time = "12:00"
 
     # Help messages
     loc_types = ["address", "station name", "station abbreviation", "coordinate"]
@@ -50,6 +59,65 @@ def get_args():
     return parser.parse_args()
 
 
+<<<<<<< Updated upstream
+=======
+def get_location(address: str) -> str:
+    """
+    Converts an address If not, to coordinates.
+    The address can be:
+        - Full name
+        - Abbreviation (maximal 4 characters and uppercase)
+        - Coordinates  (Format: "latitude, longitude")
+
+    Args:
+        address (str): Address to convert to coordinates.
+
+    Returns:
+        str: Coordinates of the address (Format: "latitude, longitude").
+    """
+    pattern = re.compile(r"^\s*-?\d+\.\d+\s*,\s*-?\d+\.\d+\s*$")
+
+    # Check if it is already a coordinate
+    if bool(pattern.match(address)):
+        return address
+
+    # Check if it is an abbreviation
+    if len(address) <= 4 and address.isupper():
+        with open(PR_STATIONS, mode="r") as file:
+            reader = csv.reader(file)
+            _ = next(reader)
+
+            for row in reader:
+                if row[1] == address:
+                    latitude = row[3]
+                    longitude = row[4]
+                    return "{}, {}".format(latitude, longitude)
+
+    # Use geopy to convert coordinates to address
+    geolocator = Nominatim(user_agent="sbb_project")
+    location = geolocator.geocode(
+        address, country_codes="ch", language="de", exactly_one=True
+    )
+
+    return "{}, {}".format(location.latitude, location.longitude)
+
+
+def get_distance(start, end):
+    """Calculate distance between two coordinates in meters.
+       Coordinates must be in the format "latitude, longitude".
+
+    Args:
+        start (str): Start coordinate.
+        end (str): End coordinate.
+
+    Returns:
+        int: Distance between the two coordinates in meters.
+    """
+    start_converted = tuple(map(float, start.split(", ")))
+    end_converted = tuple(map(float, end.split(", ")))
+    return distance(start_converted, end_converted).meters
+
+
 """
     Get the route time in seconds.
     You can use city name or coordinates.
@@ -67,9 +135,59 @@ def getRouteTime(start, end):
         'sideOfStreetDisplay' : False
     }
 
+
+def getRouteTime(start, end, method="car"):
+    endpoint = "http://www.mapquestapi.com/directions/v2/route"
+    if method not in ["foot", "bike", "car"]:
+        raise ValueError("Method must be either foot, bike or car")
+
+    names = {
+        "foot": "pedestrian",
+        "bike": "bicycle",
+        "car": "fastest",
+    }
+    # Search params
+    params = {
+        "key": "HnDX3JAuALRTge28jbZVWO1L538fJbZE",
+        "from": start,
+        "to": end,
+        "unit": "k",  # Use km instead of miles
+        "narrativeType": "none",  # Just some other parameters to omit information we don't care about
+        "sideOfStreetDisplay": False,
+        "routeType": names[method],
+    }
+
     # Do GET request and read JSON
     response = requests.get(endpoint, params=params)
     data = response.json()
     seconds = data["route"]["time"]
 
     return seconds
+
+
+def add_node(graph, node, k=5):
+    """
+    Adds a node to the graph if it doesn't exist yet.
+
+    Args:
+        graph (dict): Graph to add the node to.
+        node (???): Node to add to the graph.
+    """
+    if node in graph:
+        return
+
+    graph[node] = set()  # We assume each node is a tuple (node, distance, method)
+
+    # Now we calculate the distance between the new node and all other nodes given the 3 different methods
+    # We than take k-nearest neighbors and add them to the graph
+
+    for method in ["foot", "bike", "car"]:
+        distances = []
+        for other_node in graph:
+            if other_node == node:
+                continue
+            distance = getRouteTime(node, other_node, method)
+            distances.append((distance, other_node, method))
+
+        distances.sort(key=lambda x: x[0])
+        graph[node].update(distance[:k])
